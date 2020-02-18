9a40511635d9cbe8b15a3d4aa6eaab10cbf8db2a
Dylan Brophy <dbrophy@student.42.us.org>
2020-02-18 13:17:23 -0800

    added ft_strcpy to header

--- libft.h
+++ libft.h
@@ -12,6 +12,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 char	*ft_strdup(const char *s);
+char	*ft_strcpy(char *dest, const char *src);
 size_t	ft_strlen(const char* str);
 
 #endif
