3b882b41aa4542ed0c89e316357d5374945cc8d0
Dylan Brophy <dbrophy@student.42.us.org>
2020-02-18 08:41:28 -0800

    Added partial libft.h

---
+++ libft.h
@@ -0,0 +1,17 @@
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include "string.h"
+
+void	ft_bzero(void *s, size_t n);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char* str);
+
+#endif
