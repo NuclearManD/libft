779898a0a15cbf9707ce7e4d7cd5a5b79c5682f5
Dylan Brophy <dbrophy@student.42.us.org>
2020-02-18 22:42:50 -0800

    Corrected libft

--- libft.h
+++ libft.h
@@ -1,18 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbrophy <dbrophy@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/18 22:40:45 by dbrophy           #+#    #+#             */
+/*   Updated: 2020/02/18 22:40:45 by dbrophy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
 # include "string.h"
 
-void	ft_bzero(void *s, size_t n);
-void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
-void	*ft_memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-void	*ft_memset(void *s, int c, size_t n);
+void	ft_putstr(char *str);
+char	*ft_strlowcase(char *str);
+void	ft_putnbr(int nb);
 char	*ft_strdup(const char *s);
-char	*ft_strcpy(char *dest, const char *src);
-size_t	ft_strlen(const char* str);
+size_t	ft_strlen(const char *str);
+int		ft_str_is_lowercase(char *str);
+char	*ft_strcapitalize(char *str);
+int		ft_str_is_uppercase(char *str);
+int		ft_atoi(char *str);
+char	*ft_strupcase(char *str);
+int		ft_str_is_numeric(char *str);
+int		ft_str_is_printable(char *str);
+int		ft_str_is_alpha(char *str);
 
 #endif
