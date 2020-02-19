52818f64d1a4c817ddc20ae91b114e5f5dabfea8
Dylan Brophy <dbrophy@student.42.us.org>
2020-02-19 14:32:23 -0800

    some functions - untested, require unfinished dependencies

---
+++ ft_strjoin.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbrophy <dbrophy@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/19 12:53:22 by dbrophy           #+#    #+#             */
+/*   Updated: 2020/02/19 12:53:22 by dbrophy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char *out;
+	int i;
+
+	out = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+	if (out == NULL)
+		return (NULL);
+	i = 0;
+	while (*s1)
+		out[i++] = *(s1++);
+	while (*s2)
+		out[i++] = *(s2++);
+	return (out);
+}
---
+++ ft_strsplit.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbrophy <dbrophy@student.42.us.org>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/19 12:54:04 by dbrophy           #+#    #+#             */
+/*   Updated: 2020/02/19 12:54:04 by dbrophy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "stdlib.h"
+
+char	**ft_strsplit(char const *s, char c)
+{
+	int		nstr;
+	char	**arr;
+	char	*so;
+	char	last;
+
+	nstr = ft_strcnt_norep(s, c) + 1;
+	arr = (char**)malloc(sizeof(char*) * (nstr + 1));
+	if (arr == NULL || c == 0)
+		return NULL;
+	arr[nstr] = NULL;
+	nstr = 0;
+	last = c;
+	while (*s == c)
+		s++;
+	so = s--;
+	while (*(++s))
+	{
+		if (*s == c && c != last)
+		{
+			arr[nstr++] = ft_strcut(so, c);
+			so = ++s;
+		}
+		last = c;
+	}
+	return (arr);
+}
