5652d514e1c1f024003bb5a7d3fb7d7088bd4741
Dylan Brophy <dbrophy@student.42.us.org>
2020-02-19 00:07:17 -0800

    update libft

--- libft.h
+++ libft.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dbrophy <dbrophy@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/18 23:52:46 by dbrophy           #+#    #+#             */
-/*   Updated: 2020/02/18 23:52:46 by dbrophy          ###   ########.fr       */
+/*   Created: 2020/02/19 00:06:14 by dbrophy           #+#    #+#             */
+/*   Updated: 2020/02/19 00:06:14 by dbrophy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include "string.h"
 
 char	*ft_strrchr(char *s, int c);
+int		ft_putchar(char c);
 void	ft_putstr(char *str);
 int		ft_strcmp(char *s1, char *s2);
 int		ft_strlcat(char *dest, const char *src, unsigned int size);
@@ -24,29 +25,40 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 char	*ft_strncpy(char *dest, const char *src, unsigned int n);
 char	*ft_strlowcase(char *str);
 char	*ft_strstr(char *str, const char *to_find);
+int		ft_isdigit(int c);
+int		ft_putchar(char c);
 void	ft_putnbr(int nb);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
+int		ft_isalnum(int c);
 char	*ft_strcpy(char *dest, const char *src);
+int		ft_putchar(char c);
 void	recursive_putnbr_base(int nb, int baselen, char *base);
 void	ft_putnbr_base(int nb, char *base);
 char	*ft_strdup(const char *s);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_isascii(int c);
 size_t	ft_strlen(const char *str);
 int		ft_str_is_lowercase(char *str);
 char	*ft_strncat(char *dest, const char *src, int nb);
 char	*ft_strcapitalize(char *str);
 int		ft_str_is_uppercase(char *str);
 int		ft_strncmp(const char *s1, const char *s2, unsigned int n);
+int		calc_base_len(char *base);
+int		ft_chr_idx(char *str, int len, char c);
+int		ft_atoi_base(char *str, char *base);
 int		ft_strlcpy(char *dest, const char *src, unsigned int n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
+int		ft_isalpha(int c);
 char	*ft_strcat(char *dest, char *src);
 int		ft_atoi(char *str);
 char	*ft_strupcase(char *str);
 int		ft_str_is_numeric(char *str);
+int		ft_isprint(int c);
 int		ft_str_is_printable(char *str);
-char	*ft_strchr(const char *s, int c);
+char	*ft_strchr(char *s, int c);
 int		ft_str_is_alpha(char *str);
 
 #endif
