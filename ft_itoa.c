3d998991494afab1db0cd81e2eea61dd7de334e7
Dylan Brophy <dbrophy@student.42.us.org>
2020-02-19 15:35:37 -0800

    atoi compiles

--- ft_itoa.c
+++ ft_itoa.c
@@ -10,18 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ftlib.h"
+#include "libft.h"
 #include "stdlib.h"
 
-static int	itoa_setup(int num, char **str)
+static int	itoa_setup(int n, char **str)
 {
 	int size;
 
 	size = ft_imax(1, ft_logi(10, n)) + 1 + (n < 0);
 	*str = (char*)malloc(size);
-	str[--size] = 0;
-	if (nb == 0)
-		out[0] = '0';
+	(*str)[--size] = 0;
+	if (n == 0)
+		(*str)[0] = '0';
 	return (size);
 }
 
@@ -30,25 +30,25 @@ char		*ft_itoa(int n)
 	int		len;
 	char 	*str;
 
-	size = itoa_setup(n, &str);
+	len = itoa_setup(n, &str);
 	if (str == NULL)
 		return (NULL);
 	len = 0;
-	else if (nb == -2147483648)
+	if (n == -2147483648)
 	{
 		str[len++] = '-';
 		str[len++] = '2';
-		nb = 147483648;
+		n = 147483648;
 	}
-	else if (nb < 0)
+	else if (n < 0)
 	{
 		str[len++] = '-';
-		nb = -nb;
+		n = -n;
 	}
-	while (nb != 0)
+	while (n != 0)
 	{
-		str[len++] = (nb % 10) + '0';
-		nb = nb / 10;
+		str[len++] = (n % 10) + '0';
+		n = n / 10;
 	}
 	return (str);
 }
