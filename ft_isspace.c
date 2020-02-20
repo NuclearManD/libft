bf5cf83cb043bfbb2f26ceaa46361e6ffdd045ab
Dylan Brophy <dbrophy@student.42.us.org>
2020-02-20 10:33:39 -0800

    Standards!

--- ft_isspace.c
+++ ft_isspace.c
@@ -12,7 +12,7 @@
 
 int		ft_isspace(int c)
 {
-	if (c == ' ' || c == '\t' || c == '\n')
+	if (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r')
 		return (1);
 	return (0);
 }
