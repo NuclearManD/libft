3cf21462ec22b622f98857a1c0dd007805979c6a
Dylan Brophy <dbrophy@student.42.us.org>
2020-02-20 16:49:43 -0800

    Yay! strcmp works better now!

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -14,5 +14,5 @@ int	ft_strcmp(char *s1, char *s2)
 {
 	while ((*s1 == *(s2++)) && (*s1))
 		s1++;
-	return ((*s1) - (*(s2 - 1)));
+	return ((int)((unsigned char)(*s1) - (unsigned char)(*(s2 - 1))));
 }
