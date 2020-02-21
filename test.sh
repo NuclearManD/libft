e9968a0ce410c4f552d5d6f49348588bfecb7054
Dylan Brophy <dbrophy@student.42.us.org>
2020-02-21 13:15:16 -0800

    moved tests

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 make re
 make clean
-gcc -O0 -g tests/test.c -L . -I . -lft -o tests/test
-gcc -O0 -g tests/main.c -L . -I . -lft -o tests/maintest
+gcc -O0 -g .tests/test.c -L . -I . -lft -o .tests/test
+gcc -O0 -g .tests/main.c -L . -I . -lft -o .tests/maintest
 #lldb test
-./tests/test
+./.tests/test
 echo "Running maintest..."
-./tests/maintest
+./.tests/maintest
