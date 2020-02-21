cda37debf5662e609be46e46f743764e5df41be5
Dylan Brophy <dbrophy@student.42.us.org>
2020-02-21 14:47:25 -0800

    fyx

--- libft.h
+++ libft.h
@@ -15,12 +15,12 @@
 
 # include "string.h"
 
-typedef struct		s_list
+typedef struct	s_list
 {
 	void			*content;
 	size_t			content_size;
 	struct s_list	*next;
-}					t_list;
+}				t_list;
 
 int				ft_atoi(char *str);
 int				ft_atoi_base(char *str, char *base);
