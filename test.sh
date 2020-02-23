af819c54ca09abaa5d795216f147245d7b499566
Dylan Brophy <dbrophy@student.42.us.org>
2020-02-23 12:35:44 -0800

    Added to test file

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 make re
 make clean
 gcc -O0 -g .tests/test.c -L . -I . -lft -o .tests/test
+gcc -O0 -g .tests/vec2_and_3_tests.c -L . -I . -lft -o .tests/vec2_and_3_tests
 gcc -O0 -g .tests/main.c -L . -I . -lft -o .tests/maintest
 gcc -O0 -g .tests/mathtest.c -L . -I . -lft -o .tests/mathtest
 #lldb test
@@ -10,5 +11,7 @@ echo "Custom test:"
 ./.tests/test
 echo "Running maintest..."
 ./.tests/maintest
-echo "Math test:"
+echo "Math test 1:"
 ./.tests/mathtest
+echo "Math test 2:"
+./.tests/vec2_and_3_tests
