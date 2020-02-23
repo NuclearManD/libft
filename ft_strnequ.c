3dae59f572206a108e293966ac34a4b11265fde3
Dylan Brophy <dbrophy@student.42.us.org>
2020-02-23 01:40:50 -0800

    fix?

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -17,12 +17,12 @@ int				ft_strnequ(const char *s1, const char *s2, size_t limit)
 {
 	if (s1 == NULL || s2 == NULL)
 		return (0);
-	while (*s1 != 0 && *s2 != 0 && --limit > 0)
+	while (*s1 != 0 && *s2 != 0 && limit-- > 0)
 	{
 		if (*s1 != *s2)
 			return (0);
 		s1++;
 		s2++;
 	}
-	return (*s1 == *s2);
+	return (1);
 }
